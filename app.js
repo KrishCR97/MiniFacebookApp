cf49b30ae54c787c4ff5023cb581df863c36d3e3
Veera Kishon Kumar Mucherla <veerakishon.mucherla@gmail.com>
2018-06-22 10:21:08 -0400

    Added routes.

--- app.js
+++ app.js
@@ -1,41 +1,105 @@
 var app = angular.module("myApp", ["ngRoute"]);
+localStorage.isLoggedIn = false;
 
 app.config(function ($routeProvider) {
 
 
     $routeProvider.when('/', {
         templateUrl: '/login.html',
-        controller: 'controller1'
+        controller: 'loginController'
     }).when('/signup', {
         templateUrl: '/signup.html',
-        controller: 'controller2'
+        controller: 'signUpController'
+    }).when('/profile',{
+        templateUrl: '/profile.html',
+        controller: 'profileController',
+        resolve : ['authService',function(authService){
+            return authService.checkUserStatus();
+        }]
+    }).when('/messages',{
+        templateUrl: '/messages.html',
+        controller: 'messagesController',
+        resolve : ['authService',function(authService){
+            return authService.checkUserStatus();
+        }]
     })
 });
-
-app.controller("controller1", function ($scope, $http) {
-
-    var loginDetails = {
-        userName: $scope.userName,
-        password: $scope.password
-    }
-    $scope.checkLogin = function () {
-        $http.post("http://localhost:3000/registerUser", JSON.stringify(loginDetails)).then((data) => {
-
+var loginDetails = {
+    userName: '',
+    password: ''
+}
+app.factory("authService",function($http,$q){
+    return {
+        'checkUserStatus' : function(){
+            var defer = $q.defer();
+            $http.post("http://localhost:3000/loginValidation", JSON.stringify(loginDetails)).then((data) => {
+            console.log(data);
+            defer.resolve();
+            if (data.data.validUser) {
+                //localStorage.isLoggedIn = true;
+                defer.resolve();
+            } else {
+                //localStorage.isLoggedIn = false;
+                defer.reject();
+            }
         });
+        return defer.promise;
+        }
+    }
+
+});
+
+app.controller("loginController", function ($scope, $http) {
+
+
+    $scope.checkLogin = function () {
+        // var loginDetails = {
+        //     userName: $scope.userName,
+        //     password: $scope.password
+        // }
+        //if($scope.userName &&  $scope.password){
+        // $http.post("http://localhost:3000/loginValidation", JSON.stringify(loginDetails)).then((data) => {
+        //     console.log(data);
+        //     if (data.data.validUser) {
+        //         localStorage.isLoggedIn = true;
+        //     } else {
+        //         localStorage.isLoggedIn = false;
+        //     }
+        // });
+        //}
+        //else{
+        //    alert("Enter all the fields");
+        // }
+
     }
 });
-app.controller("controller2", function ($scope) {
-    var newUserDetails =
-    {
-        userName: "String",
-        password: "String",
-        firstName: "String",
-        lastName: "String",
-        email: "String",
-        phoneNum: 123,
-        location: "String"
-    }
+app.controller("signUpController", function ($scope, $http) {
     $scope.createNewUser = function () {
-        alert($scope.userName + " " + $scope.password + " " + $scope.firstName + " " + $scope.lastName + " " + $scope.email + " " + $scope.phNum + " " + $scope.location);
+        var newUserDetails = {
+            userName: $scope.userName,
+            password: $scope.password,
+            firstName: $scope.firstName,
+            lastName: $scope.lastName,
+            email: $scope.email,
+            phoneNum: $scope.phNum,
+            location: $scope.location
+        }
+        // if($scope.userName && $scope.password && $scope.firstName && $scope.lastName && 
+        //   $scope.email && $scope.phNum && $scope.location){
+        $http.post("http://localhost:3000/newUserRegistration", JSON.stringify(newUserDetails)).then((data) => {
+
+        });
+        //}
+        //else{
+        //    alert("Enter all the field");
+        // }
     }
+});
+
+app.controller('profileController',function($scope){
+
+});
+
+app.controller('messagesController',function($scope){
+
 });
--- server.js
+++ server.js
@@ -4,63 +4,126 @@ var mongoose = require('mongoose');
 var cors = require('cors');
 mongoose.Promise = require('q').Promise;
 const bodyParser = require('body-parser');
+
+
 app.use(cors({
-    origin : "http://127.0.0.1:5500"
+    origin: "http://127.0.0.1:5500"
 }))
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}) );
+app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb://localhost:27017/app");
-var db = mongoose.connection;
 
-db.on('open',function(){
-    console.log("Connected to mongoDB");
-});
-
-db.on('error',function(){
-console.log("Error in connected to mongoDB")
-});
 
 var user_schema = mongoose.Schema({
-    userName : String,
-    password : String,
-    firstName : String,
-    lastName : String,
-    email : String,
-    phoneNum : Number,
-    location : String
+    userName: String,
+    password: String,
+    firstName: String,
+    lastName: String,
+    email: String,
+    phoneNum: Number,
+    location: String
 });
 
-var user_model =  mongoose.model('users', user_schema);
-
-app.listen(3000,()=>{
-console.log("Server running @ 3000")
+var messages_schema = mongoose.Schema({
+    recipient: String,
+    recipient_img: String,
+    sender: String,
+    sender_img: String,
+    title: String,
+    description: String,
+    created_at: Date,
+    important: Number
 });
 
-app.get('/',(req,res)=>{
+
+var user_model = mongoose.model('users', user_schema);
+var message_model = mongoose.model('messages',messages_schema);
+
+app.listen(3000, () => {
+    mongoose.connect("mongodb://localhost:27017/app");
+    var db = mongoose.connection;
+
+    db.on('open', function () {
+        console.log("Connected to mongoDB");
+    });
+
+    db.on('error', function () {
+        console.log("Error in connected to mongoDB")
+    });
+    console.log("Server running @ 3000")
+});
+
+
+
+app.get('/', (req, res) => {
 
     var userDoc = {
-        userName : "String",
-        password : "String",
-        firstName : "String",
-        lastName : "String",
-        email : "String",
-        phoneNum : 123,
-        location : "String"
+        userName: "String",
+        password: "String",
+        firstName: "String",
+        lastName: "String",
+        email: "String",
+        phoneNum: 123,
+        location: "String"
     }
     userDoc = user_model(userDoc);
-    userDoc.save((err)=>{
-        if(!err){
+    userDoc.save((err) => {
+        if (!err) {
             console.log("Document saved");
         }
     });
     res.send({
-        firstName : "Kishon"
+        firstName: "Kishon"
     });
 });
 
-app.post('/registerUser',(req,res)=>{
-    console.log(req.body.name);
-res.send({ registered : true});
+app.get('/m',(req, res) => {
+
+    var message = {
+    recipient: "String",
+    recipient_img: "String",
+    sender: "String",
+    sender_img: "String",
+    title: "String",
+    description: "String",
+    created_at: new Date(),
+    important: 1
+    }
+    messageDoc = message_model(message);
+    messageDoc.save((err,doc) => {
+        if (!err) {
+            console.log("Document saved");
+            console.log(doc);
+        }
+    });
+    res.send({
+        firstName: "Hello"
+    });
+});
+
+app.post('/loginValidation', (req, res) => {
+    user_model.find({ userName: req.userName, password: req.password }, (err, doc) => {
+        if (doc.length == 0) {
+            res.send({ validUser: false });
+            console.log("HEhe");
+        }
+        else {
+            res.send({ validUser: true });
+        }
+    });
+});
+
+app.post('/newUserRegistration', (req, res) => {
+    var newUser = new user_model(req.body);
+    newUser.save(function (err, user) {
+        if (!err) {
+            res.send({ saveNewUser: true });
+        }
+        else {
+            res.send({ saveNewUser: false });
+        }
+
+    });
+
 });
 
